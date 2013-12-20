20ba2db7718247e4d5d1dfc4ff58d750f0dc2f69
KevM <scoundrel@gmail.com>
2013-12-19 19:08:01 -0600

    Added Rooms module and spec
    Supports getting all rooms and upserting them.

---
+++ models/Rooms.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var db = require("../lib/db");
+var Q = require("Q");
+var Room = require("./Room");
+
+function getRooms() {
+    var deferred = Q.defer();
+    db.rooms.find({}, function(error, docs) {
+        if (error) {
+            deferred.reject(new Error(error));
+        } else {
+            var rooms = docs.map(function(d) { return new Room(d); });
+            deferred.resolve(rooms);
+        }
+    });
+    return deferred.promise;
+}
+
+function saveRoom(room) {
+    var deferred = Q.defer();
+    db.rooms.update({location: room.location}, room, {upsert: true}, function(error) {
+        if (error) {
+            deferred.reject(new Error(error));
+        } else {
+            deferred.resolve(room);
+        }
+    });
+    return deferred.promise;
+}
+
+module.exports = {
+    saveRoom : saveRoom,
+    getAll : getRooms
+};
---
+++ test/rooms.spec.js
@@ -0,0 +1,82 @@
+/*global describe:false, it:false, before:false, after:false*/
+"use strict";
+
+var Rooms = require("../models/Rooms"),
+Room = require("../models/Room"),
+expect = require("chai").expect;
+
+describe("rooms", function() {
+
+    var db, rooms;
+
+    before(function(done) {
+        db = require("../lib/db");
+        db.init("./tmp");
+        rooms = [
+            { location: "lab1", name : "Fancy Lab"},
+            { location: "lab2", name : "Crappy Lab"},
+            { location: "lab3", name : "Snappy Lab"}
+        ];
+        db.rooms.insert(rooms, function(error, docs) {
+            done();
+        });
+    });
+
+    after(function(done) {
+        db._erase("./tmp");
+        done();
+    });
+
+    describe("getting all rooms", function() {
+
+        it("should return all rooms", function (done) {
+            Rooms.getAll().done(function(results) {
+                expect(results.length).to.equal(3);
+                done();
+            });
+        });
+
+    });
+
+    describe("saving a room", function() {
+
+        it("should save a room that does not already exist", function (done) {
+
+            var room = new Room({location: "Timbuk 3" });
+
+            Rooms.saveRoom(room).done(function(result) {
+                expect(result.location).to.equal(room.location);
+                done();
+            });
+        });
+
+        it("should save a room that already exists", function (done) {
+
+            var room = rooms[0];
+
+            Rooms.saveRoom(room).done(function(result) {
+                expect(result.location).to.equal(room.location);
+                expect(result.name).to.equal(room.name);
+                done();
+            });
+        });
+
+        it("should show up in room listings", function (done) {
+
+            var room = new Room({location: "Toto's Asia"});
+
+            Rooms.saveRoom(room).then(function(result) {
+                return Rooms.getAll();
+            }).done(function(roomListing) {
+
+                var results = roomListing.filter(function(r) {
+                    return r.location === room.location;
+                });
+
+                expect(results.length).to.equal(1);
+                done();
+            });
+        });
+    });
+
+});
