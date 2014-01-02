74023661283b278e84d2f5159b5705c5feb4c3f0
KevM <scoundrel@gmail.com>
2014-01-01 19:51:44 -0600

    Make jshint happy

--- controllers/student.js
+++ controllers/student.js
@@ -1,27 +1,28 @@
-'use strict';
+"use strict";
+
 var students = require("../models/students");
 var Student = require("../models/student");
 
 module.exports = function (server) {
 
-    server.get('/student/:badge', function (req, res) {
+    server.get("/student/:badge", function (req, res) {
         var badge = req.params.badge;
         students.findStudent(badge).done(function(student) {
             if(!student) {
                 res.send(404, "That user does not exist.");
             }
-            res.render('student', student);
+            res.render("student", student);
         });
     });
 
-    server.get('/student/:badge/edit', function (req, res) {
+    server.get("/student/:badge/edit", function (req, res) {
         var badge = req.params.badge;
         students.findStudent(badge).done(function(student) {
-            res.render('student-edit', student);
+            res.render("student-edit", student);
         });
     });
 
-    server.post('/student/:badge/edit', function (req, res) {
+    server.post("/student/:badge/edit'", function (req, res) {
         var badge = req.params.badge;
         var student = new Student({badge : badge, name: req.body.name});
         students.saveStudent(student).done(function(student) {
